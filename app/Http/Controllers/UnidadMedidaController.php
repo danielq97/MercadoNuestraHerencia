e7d89aaf7886788238d2a11dc28392763c29e36a
danielq97 <discover_daniel@hotmail.com>
2019-09-17 16:52:51 -0500

    Added Model and Controllers of Categoria, Usuario , Producto

---
+++ app/Http/Controllers/UnidadMedidaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Unidad_Medida;
+use Illuminate\Http\Request;
+
+class UnidadMedidaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Unidad_Medida  $unidad_Medida
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Unidad_Medida $unidad_Medida)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Unidad_Medida  $unidad_Medida
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Unidad_Medida $unidad_Medida)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Unidad_Medida  $unidad_Medida
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Unidad_Medida $unidad_Medida)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Unidad_Medida  $unidad_Medida
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Unidad_Medida $unidad_Medida)
+    {
+        //
+    }
+}
---
+++ database/migrations/2019_09_17_214601_create_unidad__medidas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUnidadMedidasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidad__medidas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unidad__medidas');
+    }
+}
