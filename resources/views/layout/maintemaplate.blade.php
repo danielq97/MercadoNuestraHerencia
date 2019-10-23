87c0bedb71cbd53e284cc975c14ffd1b38753d08
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2019-10-23 14:40:18 -0500

    funcionando registro y login

--- resources/views/layout/maintemaplate.blade.php
+++ resources/views/layout/maintemaplate.blade.php
@@ -17,40 +17,36 @@
       </head>
         <body>
             <ul class="nav justify-content-end">
-                <li class="nav-item">
-                        @if (session('status'))
-                        <a class="nav-link active" href="/login">Ingresar</a>
-                        @endif                
-                        <a class="nav-link active" href="/login">Perfil</a>
-                </li>
-                <li class="nav-item">
-                  
-                  @if (session('status'))
-                  <a class="nav-link" href="/signup">Registrarse</a>
-                  @endif                
-                  <a class="nav-link" href="{{ route('logout') }}"
-                  onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">Cerrar sesión</a>
+               
+               <!-- Authentication Links -->
+               @guest
+               <li class="nav-item">
+                   <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+               </li>
+               @if (Route::has('register'))
+                   <li class="nav-item">
+                       <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                   </li>
+               @endif
+           @else
+               <li class="nav-item dropdown">
+                   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                       {{ Auth::user()->name }} <span class="caret"></span>
+                   </a>
 
-                </li>
+                   <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                       <a class="dropdown-item" href="{{ route('logout') }}"
+                          onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                           {{ __('Logout') }}
+                       </a>
 
-                <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }} <span class="caret"></span>
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
+                       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                           @csrf
+                       </form>
+                   </div>
+               </li>
+           @endguest
 
 
 
