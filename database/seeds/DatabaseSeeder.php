dcab8955d990f01b8ddd8ece299d9d618c9ff2de
danielq97 <discover_daniel@hotmail.com>
2019-10-31 17:19:51 -0500

    changes for seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UnidadmedidaSeeder::class);
+        $this->call(UnidadmedidaSeeder::class,CategoriaSeeder::class);
+        
     }
 }
