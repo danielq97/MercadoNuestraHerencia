9c3d0da9f572defa0ebd1af495d50785a1199d6f
danielq97 <discover_daniel@hotmail.com>
2019-11-20 13:43:30 -0500

    new migration corrected

--- database/migrations/2019_11_20_184043_create_detalle_reservas_table.php
+++ database/migrations/2019_11_20_184043_create_detalle_reservas_table.php
@@ -19,7 +19,7 @@ class CreateDetalleReservasTable extends Migration
             $table->unsignedInteger('reserva_id');
             $table->foreign('reserva_id')->references('id')->on('reservas');
             $table->unsignedInteger('productoreserva_id');
-            $table->foreign('productoreserva_id')->references('id')->on('productos');
+            $table->foreign('productoreserva_id')->references('id')->on('producto_reservas');
 
             $table->timestamps();
         });
