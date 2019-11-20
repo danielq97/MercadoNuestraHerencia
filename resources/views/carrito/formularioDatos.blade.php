345f1933d7dea07e7adb482303639a745faa60b0
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2019-11-20 16:50:07 -0500

    traduccion al español de la plantilla

--- resources/views/carrito/formularioDatos.blade.php
+++ resources/views/carrito/formularioDatos.blade.php
@@ -8,53 +8,37 @@
       <div class="row justify-content-center">
         <div class="col-xl-7 ftco-animate">
                       <form action="#" class="billing-form">
-                          <h3 class="mb-4 billing-heading">Billing Details</h3>
+                          <h3 class="mb-4 billing-heading">Detalles de facturación</h3>
                 <div class="row align-items-end">
                     <div class="col-md-6">
                   <div class="form-group">
-                      <label for="firstname">Firt Name</label>
+                      <label for="firstname">Nombres</label>
                     <input type="text" class="form-control" placeholder="">
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
-                      <label for="lastname">Last Name</label>
+                      <label for="lastname">Apellidos</label>
                     <input type="text" class="form-control" placeholder="">
                   </div>
               </div>
               <div class="w-100"></div>
-                  <div class="col-md-12">
-                      <div class="form-group">
-                          <label for="country">State / Country</label>
-                          <div class="select-wrap">
-                        <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-                        <select name="" id="" class="form-control">
-                            <option value="">France</option>
-                          <option value="">Italy</option>
-                          <option value="">Philippines</option>
-                          <option value="">South Korea</option>
-                          <option value="">Hongkong</option>
-                          <option value="">Japan</option>
-                        </select>
-                      </div>
-                      </div>
-                  </div>
                   <div class="w-100"></div>
                   <div class="col-md-6">
                       <div class="form-group">
-                      <label for="streetaddress">Street Address</label>
-                    <input type="text" class="form-control" placeholder="House number and street name">
+                      <label for="streetaddress">Dirección</label>
+                    <input type="text" class="form-control" placeholder="Dirección de residencia">
                   </div>
                   </div>
                   <div class="col-md-6">
                       <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Appartment, suite, unit etc: (optional)">
+                    <input type="text" class="form-control" placeholder="Apartamento, unidad residencial, etc: (opcional)">
                   </div>
                   </div>
                   <div class="w-100"></div>
                   <div class="col-md-6">
                       <div class="form-group">
-                      <label for="towncity">Town / City</label>
+                      <label for="towncity">Barrio (de la ciudad de Palmira)</label>
                     <input type="text" class="form-control" placeholder="">
                   </div>
                   </div>
@@ -67,13 +51,13 @@
                   <div class="w-100"></div>
                   <div class="col-md-6">
                   <div class="form-group">
-                      <label for="phone">Phone</label>
+                      <label for="phone">Teléfono</label>
                     <input type="text" class="form-control" placeholder="">
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
-                      <label for="emailaddress">Email Address</label>
+                      <label for="emailaddress">Correo electronico</label>
                     <input type="text" class="form-control" placeholder="">
                   </div>
               </div>
@@ -81,8 +65,8 @@
               <div class="col-md-12">
                   <div class="form-group mt-4">
                                       <div class="radio">
-                                        <label class="mr-3"><input type="radio" name="optradio"> Create an Account? </label>
-                                        <label><input type="radio" name="optradio"> Ship to different address</label>
+                                        <label class="mr-3"><input type="radio" name="optradio"> Paso a recoger el producto </label>
+                                        <label><input type="radio" name="optradio"> Pagar al recibir el producto</label>
                                       </div>
                                   </div>
               </div>
@@ -93,18 +77,14 @@
             <div class="row mt-5 pt-3">
                 <div class="col-md-12 d-flex mb-5">
                     <div class="cart-detail cart-total p-3 p-md-4">
-                        <h3 class="billing-heading mb-4">Cart Total</h3>
+                        <h3 class="billing-heading mb-4">Total</h3>
                         <p class="d-flex">
                                   <span>Subtotal</span>
                                   <span>$20.60</span>
                               </p>
                               <p class="d-flex">
-                                  <span>Delivery</span>
+                                  <span>Domicilio</span>
                                   <span>$0.00</span>
-                              </p>
-                              <p class="d-flex">
-                                  <span>Discount</span>
-                                  <span>$3.00</span>
                               </p>
                               <hr>
                               <p class="d-flex total-price">
