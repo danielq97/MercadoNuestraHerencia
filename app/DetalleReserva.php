2e6fe4809936f9e1f0fa507757901a36d7476183
danielq97 <discover_daniel@hotmail.com>
2019-11-20 12:52:56 -0500

    métodos para efectuar reserva

--- app/DetalleReserva.php
+++ app/DetalleReserva.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class DetalleReserva extends Model
 {
     //
+    //Método para obtener la reserva de un detalle
+    public function getReserva(){
+        return $this->belongsTo('App\Reserva');
+    }
+
+    //Método para obtener el producto de un detalle
+    public function getProducto(){
+        return $this -> belongsTo(('App\Producto'));
+    }
+
 }
--- app/Reserva.php
+++ app/Reserva.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Reserva extends Model
 {
     //
+
+    //Método para obtener todos los detalles de una reserva
+    public function getDetails(){
+        return $this->hasMany('App\DetalleReserva');
+    }
 }
