7e03a752332fb3c42c4e040a31a5fc29cacec587
danielq97 <discover_daniel@hotmail.com>
2019-11-21 12:24:06 -0500

    updated filter by categories for detail

--- app/Http/Controllers/ProductoDetailController.php
+++ app/Http/Controllers/ProductoDetailController.php
@@ -11,12 +11,12 @@ class ProductoDetailController extends Controller
     
     public function index($id) {
         $producto = Producto::findOrFail($id);        
-        $unidadmedida = Unidad_Medida::findOrFail( $producto->unidadmedida_id   );
+        $unidadmedida = Unidad_Medida::findOrFail( $producto->unidadmedida_id );
         //$productosPorCategoria=array();
-        $productosPorCategoria['productosPorCategoria'] = Producto::where ('categoria_id','=',$producto->categoria_id); 
+        $productosPorCategoria['productosPorCategoria'] = Producto::where (['categoria_id','=',$producto->categoria_id],['nombre','!=',$producto->nombre]); 
         return view('/productos/detail',compact('producto'),compact('unidadmedida','productosPorCategoria'));
 
-        
+
      }
      public function create() {
         echo 'create';
