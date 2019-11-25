edafe080b6d3c3311b3d4f446618b9f9df87dd1f
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2019-11-25 00:55:18 -0500

    cambiada tabla headers por elementos de tabla

--- resources/views/adminViews/pedidosDisponibles.blade.php
+++ resources/views/adminViews/pedidosDisponibles.blade.php
@@ -23,14 +23,13 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-
+              
                 @foreach ($reservas as $reserva)
               <tr class="text-center">
                 <td class="addres">{{ $reserva->direccion }}</td>
                 <td class="phone">{{ $reserva->telefono }}</td>
                 <td class="date">{{ $reserva->fecha }}</td>
-                <td class="totalprice">{{ $reserva->precio_total }}</td>
+                <td class="totalprice">$ {{ $reserva->precio_total }}</td>
                 <td class="domicilio">{{ $reserva->domicilio }}</td>
                 <td class="status">{{ $reserva->estado }}</td>
                 <td class="user">{{ $reserva->usuario_id }}</td>
